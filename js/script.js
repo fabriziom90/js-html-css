70f0e934f5ad7fb747000a4ab28d1960dfb170af
fabriziom90 <fabriziomastrobattista@gmail.com>
2024-06-26 10:58:32 +0200

    [UPD] Added querySelector definition

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-//1 - getElementById('MIO_ID'): seleziona un elemento attraverso il suo id
+// 1 - getElementById('MIO_ID'): seleziona un elemento attraverso il suo id
 let h2 = document.getElementById('title-card');
 console.log(h2);
+
+//2 - querySelector('.classe'): seleziona il primo elemento avente una determinata classe;
+let square = document.querySelector('.square.gold'); //all'interno delle parentesi uso la sintassi di CSS. Anche per i selettori avanzati
+console.log(square);
 
 
